b2383ae02e7ef58cb4b4b48efb31f9a590fb23ed
Sun Kim <sunkim0205@gmail.com>
2025-05-01 23:27:32 -0500

    forgot to save a file

--- mini-project/src/utils/calculateBusinessDays.js
+++ mini-project/src/utils/calculateBusinessDays.js
@@ -1,6 +1,6 @@
 export const calculateBusinessDays = (startDate, endDate) => {
-  const start = new Date(startDate);
-  const end = new Date(endDate);
+  const start = new Date(startDate + "T00:00:00");
+  const end = new Date(endDate + "T00:00:00");
   let days = 0;
 
   for (let d = new Date(start); d <= end; d.setDate(d.getDate() + 1)) {
